b354a71ff64c02474927f839112d10dbe81f4268
105624033 <behzad.b.i.g@gmail.com>
2025-06-02 08:42:34 +0330

    cmt

---
+++ tailwind.config.ts
@@ -0,0 +1,94 @@
+// import type { Config } from "tailwindcss";
+// const config: Config = {
+//   content: [
+//     "./app/**/*.{js,ts,jsx,tsx}",
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily:{
+//         sans: ["var(--font-geist-sans)", "sans-serif"],
+//         mono: ["var(--font-geist-mono)", "monospace"],
+//      },
+//       colors: {
+//         brand: "#1da1f2",
+//         'brand-bg': "#09111f",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+// export default config;
+
+import type { Config } from 'tailwindcss';
+import defaultTheme from "tailwindcss/defaultTheme"
+import { handler } from 'tailwindcss-animate'
+
+const config = {
+  darkMode: ['class',''],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+        xs: '360px',
+      },
+    },
+    extend: {
+      colors: {
+        blue: {
+          100: '#B4C6EE',
+          400: '#417BFF',
+          500: '#3371FF',
+        },
+        red: {
+          400: '#DD4F56',
+          500: '#DC4349',
+        },
+        dark: {
+          100: '#09111F',
+          200: '#0B1527',
+          300: '#0F1C34',
+          350: '#12213B',
+          400: '#27344D',
+          500: '#2E3D5B',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      backgroundImage: {
+        doc: 'url(/assets/images/doc.png)',
+        modal: 'url(/assets/images/modal.png)',
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  
+  plugins: [handler],
+} satisfies Config;
+
+export default config;
